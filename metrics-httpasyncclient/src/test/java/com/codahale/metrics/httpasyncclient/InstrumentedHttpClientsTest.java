675946d79a07488ee60bd13dbf07327f8f7458e0
Stefan Saasen <ssaasen@atlassian.com>
2015-11-06 12:07:09 +1100

    httpasyncclient: Fix instrumentation of the InstrumentedNHttpClientBuilder
    Previously the Timer only measured the creation of the Future but not
    the execution of the request.
    In order to time the actual request execution a custom `FutureCallback` is used.
    
    Add a separate TimerTest that ensures that the Timer is stopped
    correctly (i.e. after the response is received and not after the Future
    is created).
    
    This change might have a significant impact on current consumers as the
    scope of what is measured changes.

--- metrics-httpasyncclient/src/test/java/com/codahale/metrics/httpasyncclient/InstrumentedHttpClientsTest.java
+++ metrics-httpasyncclient/src/test/java/com/codahale/metrics/httpasyncclient/InstrumentedHttpClientsTest.java
@@ -4,28 +4,32 @@ import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.MetricRegistryListener;
 import com.codahale.metrics.Timer;
 import com.codahale.metrics.httpclient.HttpClientMetricNameStrategy;
-import com.codahale.metrics.httpclient.InstrumentedHttpClients;
 import org.apache.http.HttpRequest;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.nio.client.HttpAsyncClient;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class InstrumentedHttpClientsTest {
-    private final HttpClientMetricNameStrategy metricNameStrategy =
-            mock(HttpClientMetricNameStrategy.class);
-    private final MetricRegistryListener registryListener =
-            mock(MetricRegistryListener.class);
+
+    @Mock
+    private HttpClientMetricNameStrategy metricNameStrategy;
+
+    @Mock
+    private MetricRegistryListener registryListener;
+
     private final MetricRegistry metricRegistry = new MetricRegistry();
-    
-    private  HttpAsyncClient hac;
+
+    private HttpAsyncClient hac;
 
     @Before
     public void setUp() throws Exception {
